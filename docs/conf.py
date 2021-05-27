fc4764b70d1b0357d4ce8680078366542b19765b
Lukas Westhofen <lukas.westhofen@offis.de>
2021-05-27 09:30:24 +0200

    Add MathJax as sphinx extension

--- docs/conf.py
+++ docs/conf.py
@@ -1,3 +1,5 @@
+extensions = ["sphinx.ext.mathjax"]
+
 project = "Criticality Metrics"
 author = "Lukas Westhofen, Christian Neurohr, Tjark Koopmann, Martin Butz, Barbara Ulrike Schütt, Fabian Utesch, Birte Kramer, Christian Gutenkunst, Eckard Böde"
 
